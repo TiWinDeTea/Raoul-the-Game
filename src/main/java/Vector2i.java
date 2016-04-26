01edf39b0bf132c6e05a2f2a654355e695109ce3
Lucas Lazare <lazarelucas@yahoo.fr>
2016-04-26 13:10:35 +0200

    Changing architecture and updating diagram

---
+++ src/main/java/Vector2i.java
@@ -0,0 +1,29 @@
+/**
+ * Created by organic-code on 4/17/16.
+ */
+public class Vector2i {
+    public int x, y;
+
+    public Vector2i() {
+    }
+
+    public Vector2i(Vector2i v) {
+        this.x = v.x;
+        this.y = v.y;
+    }
+
+    public Vector2i(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(Vector2i vect) {
+        return this.x == vect.x && this.y == vect.y;
+    }
+}
