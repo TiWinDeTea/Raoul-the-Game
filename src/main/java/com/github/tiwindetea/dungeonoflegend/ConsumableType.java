c5625f000ef632ecca82f641179c867cae87957b
pinam45 <maxime.pin@live.fr>
2016-04-27 11:36:04 +0200

    Adding toString methods to enums

--- src/main/java/com/github/tiwindetea/dungeonoflegend/ConsumableType.java
+++ src/main/java/com/github/tiwindetea/dungeonoflegend/ConsumableType.java
@@ -1,14 +1,30 @@
 package com.github.tiwindetea.dungeonoflegend;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Created by maxime on 4/23/16.
  */
 public enum ConsumableType {
-	POT,
-	SCROLL;
+	POT {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("pot");
+		}
+	},
+	SCROLL {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("scroll");
+		}
+	};
+
+	private final static String bundleName = "ConsumableType";
+
+	private final static ResourceBundle resourceBundle = ResourceBundle.getBundle(bundleName, Locale.getDefault());
 
 	public String toString() {
-		//TODO
 		return null;
 	}
 }
--- src/main/java/com/github/tiwindetea/dungeonoflegend/WeaponType.java
+++ src/main/java/com/github/tiwindetea/dungeonoflegend/WeaponType.java
@@ -1,16 +1,43 @@
 package com.github.tiwindetea.dungeonoflegend;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Created by maxime on 4/23/16.
  */
 public enum WeaponType {
-	BOW,
-	SWORD,
-	WAND,
-	FIST;
+	BOW {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("bow");
+		}
+	},
+	SWORD {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("sword");
+		}
+	},
+	WAND {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("wand");
+		}
+	},
+	FIST {
+		@Override
+		public String toString() {
+			return resourceBundle.getString("fist");
+		}
+	};
+
+	private final static String bundleName = "WeaponType";
+
+	private final static ResourceBundle resourceBundle = ResourceBundle.getBundle(bundleName, Locale.getDefault());
+
 
 	public String toString() {
-		//TODO
 		return null;
 	}
 }
