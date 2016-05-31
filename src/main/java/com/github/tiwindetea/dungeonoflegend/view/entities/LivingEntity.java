452452add95b838b4658be683bb0a4969e260fa5
pinam45 <maxime.pin@live.fr>
2016-05-31 18:34:55 +0200

    Adding damage display on LivingEntity

--- src/main/java/com/github/tiwindetea/dungeonoflegend/view/entities/LivingEntity.java
+++ src/main/java/com/github/tiwindetea/dungeonoflegend/view/entities/LivingEntity.java
@@ -4,10 +4,19 @@ import com.github.tiwindetea.dungeonoflegend.model.Direction;
 import com.github.tiwindetea.dungeonoflegend.model.Vector2i;
 import com.github.tiwindetea.dungeonoflegend.view.RectangleSizeTransition;
 import com.github.tiwindetea.dungeonoflegend.view.ViewPackage;
+import com.sun.javafx.tk.Toolkit;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.util.Duration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * The type LivingEntity.
@@ -21,6 +30,12 @@ public class LivingEntity extends Entity {
 	private static final Color MAX_HEALTH_RECTANGLE_COLOR = Color.MAROON;
 	private static final Color ACTUAL_HEALTH_RECTANGLE_COLOR = Color.RED;
 	private static final Duration ANIMATION_DURATION = Duration.millis(500);
+
+	private static final Font TEXT_FONT = Font.font("Arial", FontWeight.NORMAL, 14);
+	private static final Color TEXT_COLOR = Color.RED;
+	private static final Duration TRANSITION_DURATION = new Duration(1500);
+	private static final double TRANSITION_INITIAL_Y_POSITION = -12;
+	private static final double TRANSITION_FINAL_Y_POSITION = -60;
 
 	private final Rectangle maxHealthRectangle = new Rectangle(HEALTH_RECTANGLES_WIDTH, HEALTH_RECTANGLES_HEIGHT,MAX_HEALTH_RECTANGLE_COLOR);
 	private final Rectangle actualHealthRectangle = new Rectangle(HEALTH_RECTANGLES_WIDTH, HEALTH_RECTANGLES_HEIGHT,ACTUAL_HEALTH_RECTANGLE_COLOR);
@@ -112,5 +127,48 @@ public class LivingEntity extends Entity {
 	@Override
 	public boolean isVisibleOnFog() {
 		return false;
+	}
+
+	public void displayDamages(int damages) {
+		Label label = new Label(Integer.toString(damages));
+		label.setFont(TEXT_FONT);
+		label.setTextFill(TEXT_COLOR);
+		label.impl_processCSS(true);
+		label.setTranslateX((ViewPackage.SPRITES_SIZE.x - Toolkit.getToolkit().getFontLoader().computeStringWidth(label.getText(), label.getFont())) / 2);
+
+		getChildren().add(label);
+
+		FadeTransition fadeTransition = new FadeTransition();
+		fadeTransition.setNode(label);
+		fadeTransition.setDuration(TRANSITION_DURATION);
+		fadeTransition.setFromValue(1.0);
+		fadeTransition.setToValue(0.0);
+		fadeTransition.setCycleCount(1);
+		fadeTransition.setAutoReverse(false);
+
+		TranslateTransition translateTransition = new TranslateTransition();
+		translateTransition.setNode(label);
+		translateTransition.setDuration(TRANSITION_DURATION);
+		translateTransition.setFromY(TRANSITION_INITIAL_Y_POSITION);
+		translateTransition.setToY(TRANSITION_FINAL_Y_POSITION);
+		translateTransition.setCycleCount(1);
+		translateTransition.setAutoReverse(false);
+
+		fadeTransition.play();
+		translateTransition.play();
+
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		executorService.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(2000);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				getChildren().remove(label);
+			}
+		});
+		executorService.shutdown();
 	}
 }
