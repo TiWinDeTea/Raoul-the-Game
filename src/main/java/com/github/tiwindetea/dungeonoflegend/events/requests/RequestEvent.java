b1db60bc1c77f07c6be998c105cf35795bd33746
pinam45 <maxime.pin@live.fr>
2016-06-05 17:35:56 +0200

    Adding missing extend Event

--- src/main/java/com/github/tiwindetea/dungeonoflegend/events/requests/RequestEvent.java
+++ src/main/java/com/github/tiwindetea/dungeonoflegend/events/requests/RequestEvent.java
@@ -1,11 +1,12 @@
 package com.github.tiwindetea.dungeonoflegend.events.requests;
 
+import com.github.tiwindetea.dungeonoflegend.events.Event;
 import com.github.tiwindetea.dungeonoflegend.events.EventType;
 
 /**
  * Created by maxime on 5/6/16.
  */
-public abstract class RequestEvent {
+public abstract class RequestEvent extends Event {
     public abstract RequestEventType getSubType();
 
     public EventType getType() {
