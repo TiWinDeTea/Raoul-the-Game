4205074dd169472291b1cd74c61e2b5ebc944462
pinam45 <maxime.pin@live.fr>
2016-06-05 17:43:03 +0200

    Adding licence on files where it was missing

--- src/main/java/com/github/tiwindetea/dungeonoflegend/listeners/game/map/TileModificationListener.java
+++ src/main/java/com/github/tiwindetea/dungeonoflegend/listeners/game/map/TileModificationListener.java
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                                                                              //
+//     This Source Code Form is subject to the terms of the Mozilla Public      //
+//     License, v. 2.0. If a copy of the MPL was not distributed with this      //
+//     file, You can obtain one at http://mozilla.org/MPL/2.0/.                 //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+
 package com.github.tiwindetea.dungeonoflegend.listeners.game.map;
 
 import com.github.tiwindetea.dungeonoflegend.events.map.TileModificationEvent;
